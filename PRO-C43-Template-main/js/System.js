3d7eb57d5b7439712822be03a774f484e12c110a
maahilshah23 <84512885+maahilshah23@users.noreply.github.com>
2021-05-20 18:29:56 +0530

    Add files via upload

---
+++ PRO-C43-Template-main/js/System.js
@@ -0,0 +1,16 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        //add code to return true if correct answer entered.
+
+        textSize(50);
+        fill("black");    
+        text(code,200,200)
+    }
+
+}
